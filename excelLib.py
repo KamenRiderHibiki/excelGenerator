6f22f7bf4ea906c855fde43463c5eaa084ef5c08
KamenRiderHibiki <60097232+KamenRiderHibiki@users.noreply.github.com>
2021-01-11 15:21:29 +0800

    Add files via upload

---
+++ excelLib.py
@@ -0,0 +1,165 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import os
+
+
+def typeof(variate) -> str:
+    """
+    判断变量类型
+    """
+    type = None
+    if isinstance(variate, int):
+        type = "int"
+    elif isinstance(variate, str):
+        type = "str"
+    elif isinstance(variate, float):
+        type = "float"
+    elif isinstance(variate, list):
+        type = "list"
+    elif isinstance(variate, tuple):
+        type = "tuple"
+    elif isinstance(variate, dict):
+        type = "dict"
+    elif isinstance(variate, set):
+        type = "set"
+    return type
+
+
+def getType(variate):
+    """
+    返回变量类型
+    """
+    arr = {"int": "整数", "float": "浮点", "str": "字符串", "list": "列表", "tuple": "元组", "dict": "字典", "set": "集合"}
+    vartype = typeof(variate)
+    if not (vartype in arr):
+        return "未知类型"
+    return arr[vartype]
+
+
+def createNameStrs(prevName=None, midName=None, nums=[1]*1, rjust=0, afterName=None) -> list:
+    """
+    创建名字序列；
+    params:前缀、连接符、循环列表、对齐、后缀
+    """
+    resultStrs = []
+    string = ''
+    if isinstance(nums, list):
+        numeration = True
+    elif isinstance(nums, int):
+        numeration = False
+        nums = [nums]
+    if len(nums) > 0:
+        for i in range(len(nums)):
+            for j in range(nums[i]):
+                string = (prevName if prevName else '') + (str(i + 1) if numeration else '') + \
+                         (midName if midName else '') + str(j + 1).rjust(rjust, '0') + \
+                         (afterName if afterName else '')
+                resultStrs.append(string)
+    else:
+        pass
+    return resultStrs
+
+
+def loadExcel(path=None):
+    """
+    加载excel
+    """
+    excel = None
+    if path is not None:
+        excel = load_workbook(path)
+    return excel
+
+
+def createExcels(wb: Workbook, path=None, excelNames=[]):
+    """
+    保存批量复制的excel；
+    params:excel，路径，名称列表
+    """
+    if excelNames != []:
+        for i in range(len(excelNames)):
+            wb.save((path if path else '') + '\\' + excelNames[i] + '.xlsx')
+    return
+
+
+def createSheets(wb: Workbook, sheetNames=[], deletePrev=True):
+    """
+    创建sheet；
+    params:excel，名称列表，清除旧有数据flag
+    """
+    existNames = wb.sheetnames
+    if isinstance(sheetNames, list):
+        pass
+    elif isinstance(sheetNames, str):
+        sheetNames = [sheetNames]
+    if sheetNames != []:
+        for i in range(len(sheetNames)):
+            if sheetNames[i] not in existNames:
+                wb.create_sheet(sheetNames[i])
+    if deletePrev:
+        for i in range(len(existNames)):
+            if existNames[i] not in sheetNames:
+                del wb[existNames[i]]
+    return
+
+
+def insertDataToPos(wb: Workbook, row, col, value, sheetNames=None):
+    """
+    在指定的sheet内插入数据；
+    params:excel，行，列，值，名称列表
+    """
+    if sheetNames is None:
+        sheetNames = wb.sheetnames
+    elif isinstance(sheetNames, str):
+        sheetNames = [sheetNames]
+    elif isinstance(sheetNames, list):
+        pass
+    else:
+        return False
+    sheetNames = [i for i in sheetNames if i in wb.sheetnames]
+
+    if isinstance(value, str):
+        value = [value]*len(sheetNames)
+    elif isinstance(value, list):
+        if len(value) < len(sheetNames):
+            affixArr = [value[-1]]*(len(sheetNames)-len(value))
+            value.extend(affixArr)
+    else:
+        return False
+    for i in range(len(sheetNames)):
+        sh = wb[sheetNames[i]]
+        sh.cell(row, col, value[i])
+    return True
+
+
+def makeDir(path):
+    """
+    创建目录
+    """
+    # 移除字符串头尾的字符（默认为空格或换行符）
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
+    if os.path.exists(path):
+        # 如果目录存在则不创建，并提示目录已存在
+        print(path + '目录已存在')
+        return False
+    else:
+        # 如果不存在则创建目录
+        os.makedirs(path)
+        return True
+
+
+def removeDir(path):
+    """
+    移除目录
+    """
+    for i in os.listdir(path):
+        path_file = os.path.join(path, i)
+        if os.path.isfile(path_file):
+            os.remove(path_file)
+        else:
+            removeDir(path_file)
+    # os.removedirs()会逐级删除到不能再删除，直接删了本来之后删除的父文件夹导致报FileNotFoundError，和函数的递归逻辑不相容
+    # 如果去掉rmdir()可以实现删除文件而不删除文件夹
+    os.rmdir(path)
+    return
